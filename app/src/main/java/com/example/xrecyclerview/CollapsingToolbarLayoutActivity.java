799c576310b7dceb0bf9d9f136100a9db11379a0
cwl <18159412113@163.com>
2019-04-01 14:00:19 +0800

    newbranch

--- app/src/main/java/com/example/xrecyclerview/CollapsingToolbarLayoutActivity.java
+++ app/src/main/java/com/example/xrecyclerview/CollapsingToolbarLayoutActivity.java
@@ -13,7 +13,7 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;说说
+import android.widget.Toast;111
 
 import com.jcodecraeer.xrecyclerview.ProgressStyle;
 import com.jcodecraeer.xrecyclerview.XRecyclerView;
